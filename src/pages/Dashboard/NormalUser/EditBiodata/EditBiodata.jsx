2a04d7d95f0f02ebf459336326f6fa6257ea1359
limon <limonsk026@gmail.com>
2025-01-16 15:49:06 +0600

    Edit biodata form design complte

--- src/pages/Dashboard/NormalUser/EditBiodata/EditBiodata.jsx
+++ src/pages/Dashboard/NormalUser/EditBiodata/EditBiodata.jsx
@@ -1,6 +1,7 @@
 import SectionTitle from "../../../../components/SectionTitle";
 import { useForm } from "react-hook-form";
 import EditBiodataForm from "./EditBiodataForm";
+import { imageUpload } from "../../../../utility/utility";
 
 const EditBiodata = () => {
   const {
@@ -8,7 +9,12 @@ const EditBiodata = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    const age = parseInt(data.age);
+    const image = await imageUpload(data.image[0]);
+    console.log({...data,age,image});
+    
+  };
   return (
     <div className=" min-h-screen bg-gray-100 flex flex-col items-center justify-center py-10">
       <SectionTitle heading="Edit Your Biodata"></SectionTitle>
--- src/pages/Dashboard/NormalUser/EditBiodata/EditBiodataForm.jsx
+++ src/pages/Dashboard/NormalUser/EditBiodata/EditBiodataForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import useAuth from "../../../../hooks/useAuth";
 
 const EditBiodataForm = ({ handleSubmit, onSubmit, register }) => {
+  const { user } = useAuth();
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
@@ -15,9 +17,11 @@ const EditBiodataForm = ({ handleSubmit, onSubmit, register }) => {
           required
           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
         >
-          <option value="">Select Type</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
+          <option value="" disabled>
+            Select Type
+          </option>
+          <option>Male</option>
+          <option>Female</option>
         </select>
       </div>
 
@@ -46,12 +50,12 @@ const EditBiodataForm = ({ handleSubmit, onSubmit, register }) => {
         />
       </div>
 
-      {/* date Input */}
+      {/* date of birth Input */}
       <div>
         <label className="block font-medium text-gray-700">Date of Birth</label>
         <input
           type="date"
-          {...register("date")}
+          {...register("dateOfBirth")}
           required
           id="name"
           placeholder="Enter your full name"
@@ -59,7 +63,7 @@ const EditBiodataForm = ({ handleSubmit, onSubmit, register }) => {
         />
       </div>
 
-      {/* height type */}
+      {/* height  */}
       <div>
         <label className="block font-medium text-gray-700">Height</label>
         <select
@@ -68,12 +72,235 @@ const EditBiodataForm = ({ handleSubmit, onSubmit, register }) => {
           required
           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
         >
-          <option value="">Select height</option>
+          <option value="" disabled>
+            Select height
+          </option>
           <option>5.3</option>
           <option>5.4</option>
           <option>4.3</option>
           <option>4.7</option>
         </select>
+      </div>
+
+      {/* weight  */}
+      <div>
+        <label className="block font-medium text-gray-700">Weight</label>
+        <select
+          defaultValue=""
+          {...register("weight")}
+          required
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        >
+          <option value="" disabled>
+            Select weight
+          </option>
+          <option>40</option>
+          <option>50</option>
+          <option>60</option>
+          <option>45</option>
+        </select>
+      </div>
+
+      {/* age Input */}
+      <div>
+        <label className="block font-medium text-gray-700">Age</label>
+        <input
+          type="number"
+          {...register("age")}
+          required
+          placeholder="Enter your age"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+      </div>
+
+      {/* occupation  */}
+      <div>
+        <label className="block font-medium text-gray-700">Occupation</label>
+        <select
+          defaultValue=""
+          {...register("occupation")}
+          required
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        >
+          <option value="" disabled>
+            Select Occupation
+          </option>
+          <option>Doctor</option>
+          <option>Engineer</option>
+          <option>Teacher</option>
+        </select>
+      </div>
+
+      {/* race or skin color  */}
+      <div>
+        <label className="block font-medium text-gray-700">Race</label>
+        <select
+          defaultValue=""
+          {...register("race")}
+          required
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        >
+          <option value="" disabled>
+            Select Race
+          </option>
+          <option>Light</option>
+          <option>Medium</option>
+          <option>Dark</option>
+        </select>
+      </div>
+
+      {/* Father's name */}
+      <div>
+        <label className="block font-medium text-gray-700">Fathers Name</label>
+        <input
+          type="text"
+          {...register("fathersName")}
+          required
+          placeholder="Enter your fathers name"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+      </div>
+
+      {/* Mother's name */}
+      <div>
+        <label className="block font-medium text-gray-700">Mothers name</label>
+        <input
+          type="text"
+          {...register("mothersName")}
+          required
+          placeholder="Enter your mothers name"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+      </div>
+
+      {/* parmanent division */}
+      <div>
+        <label className="block font-medium text-gray-700">
+          Permanent Division name
+        </label>
+        <select
+          defaultValue=""
+          {...register("permanentDivision")}
+          required
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        >
+          <option value="" disabled>
+            Select Division
+          </option>
+          <option>Dhaka</option>
+          <option>Chattogram</option>
+          <option>Rangpur</option>
+          <option>Barisal</option>
+          <option>Khulna</option>
+          <option>Mymensingh</option>
+          <option>Sylhet</option>
+        </select>
+      </div>
+
+      {/* present division */}
+      <div>
+        <label className="block font-medium text-gray-700">
+          Present Division name
+        </label>
+        <select
+          defaultValue=""
+          {...register("presentDivision")}
+          required
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        >
+          <option value="" disabled>
+            Select Division
+          </option>
+          <option>Dhaka</option>
+          <option>Chattogram</option>
+          <option>Rangpur</option>
+          <option>Barisal</option>
+          <option>Khulna</option>
+          <option>Mymensingh</option>
+          <option>Sylhet</option>
+        </select>
+      </div>
+
+      {/*  excepted partner age */}
+      <div>
+        <label className="block font-medium text-gray-700">
+          Expected Partner Age
+        </label>
+        <input
+          type="number"
+          {...register("partnerAge")}
+          required
+          placeholder="Enter your partner age"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+      </div>
+
+      {/* excepted partner height  */}
+      <div>
+        <label className="block font-medium text-gray-700">
+          Expected Partner Height
+        </label>
+        <select
+          defaultValue=""
+          {...register("partnerHeight")}
+          required
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        >
+          <option value="" disabled>
+            Select height
+          </option>
+          <option>5.3</option>
+          <option>5.4</option>
+          <option>4.3</option>
+          <option>4.7</option>
+        </select>
+      </div>
+
+      {/* excepted partner weight  */}
+      <div>
+        <label className="block font-medium text-gray-700">
+          Expected Partner Weight
+        </label>
+        <select
+          defaultValue=""
+          {...register("partnerWeight")}
+          required
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        >
+          <option value="" disabled>
+            Select weight
+          </option>
+          <option>40</option>
+          <option>50</option>
+          <option>60</option>
+          <option>45</option>
+        </select>
+      </div>
+
+      {/* contact email */}
+      <div>
+        <label className="block font-medium text-gray-700">Contact Email</label>
+        <input
+          type="email"
+          {...register("email")}
+          readOnly
+          defaultValue={user?.email}
+          required
+          placeholder="Enter your mothers name"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+      </div>
+
+      {/*  mobile numbre */}
+      <div>
+        <label className="block font-medium text-gray-700">Mobile Number</label>
+        <input
+          type="number"
+          {...register("mobileNumber")}
+          required
+          placeholder="Enter your partner age"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500"
+        />
       </div>
 
       {/* Submit Button */}
