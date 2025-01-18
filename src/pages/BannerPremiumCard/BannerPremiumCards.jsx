fd0188d471d1007337497d2763c511848b173bbc
limon <limonsk026@gmail.com>
2025-01-18 12:10:49 +0600

    biodata details page design compelete

--- src/pages/BannerPremiumCard/BannerPremiumCards.jsx
+++ src/pages/BannerPremiumCard/BannerPremiumCards.jsx
@@ -1,12 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import PremiumCard from "../../components/PremiumCard";
 import SectionTitle from "../../components/SectionTitle";
-
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const BannerPremiumCards = () => {
-  
+  const axiosPublic = useAxiosPublic();
+  const { data: biodatas = [] } = useQuery({
+    queryKey: ["biodata"],
+    queryFn: async () => {
+      const { data } = await axiosPublic("/biodata");
+      return data;
+    },
+  });
   // handle age short
   const handlSort = (order) => {
-     console.log(order)
+    console.log(order);
     // const sortedData = [...data].sort((a, b) =>
     //   order === "asc" ? a.age - b.age : b.age - a.age
     // );
@@ -15,15 +23,17 @@ const BannerPremiumCards = () => {
     <div>
       <SectionTitle heading="Biodata Premium "></SectionTitle>
       <p className="text-2xl font-bold  mt-3">
-        Sort By Age : 
+        Sort By Age :
         <select className="ml-3" onChange={(e) => handlSort(e.target.value)}>
           <option value="asc">Min Age to Max Age</option>
           <option value="desc">Max Age to Min Age</option>
         </select>
       </p>
       {/* card design */}
-      <div className="w-11/12 mx-auto">
-      <PremiumCard></PremiumCard>
+      <div className="w-11/12 mx-auto grid grid-cols-1 my-10 md:grid-cols-2 gap-3">
+        {biodatas.map((biodata) => (
+          <PremiumCard key={biodata._id} biodata={biodata}></PremiumCard>
+        ))}
       </div>
     </div>
   );
--- src/pages/BiodataDetails/BiodataDetails.jsx
+++ src/pages/BiodataDetails/BiodataDetails.jsx
@@ -1,14 +1,125 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import daimond from "../../assets/daimond.png";
+import card from "../../assets/card.png";
 
 const BiodataDetails = () => {
-    const data = useLoaderData();
-    console.log(data)
-    return (
-        <div>
-            
+  const biodata = useLoaderData();
+  console.log(biodata);
+  return (
+    <div className="bg-[#F2F2F2] py-10 mt-2">
+      <div className=" flex flex-col items-center gap-5 md:flex-row md:justify-start md:items-center md:px-20">
+        {/* Profile Picture */}
+        <img
+          src="https://i.ibb.co/SRJ5dPV/myprofile.jpg"
+          className="w-36 h-36  shadow-lg"
+          alt="Profile"
+        />
+        {/* Name and ID */}
+        <div className="text-center md:text-left">
+          <p className="text-2xl font-bold text-gray-800">
+            Name: <span className="text-gray-500">Md Limon</span>
+          </p>
+          <p className="text-xl font-medium text-gray-700">ID: 01234</p>
+          <p className="text-xl font-medium text-gray-700">
+            Biodata Type: {biodata.biodataType}
+          </p>
+          <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 gap-2 mt-2"
+          >
+            <img src={card} className="w-7" alt="" />
+            Add to Favourites 
+          </button>
         </div>
-    );
+      </div>
+
+      {/* Biodata Details */}
+      <div className="grid grid-cols-1 gap-6 mt-10 px-5 md:px-20">
+        {/* Basic details card */}
+        <div className="w-full bg-white shadow-lg px-8 py-5 rounded-lg">
+          <h1 className="text-2xl text-center">Basic Details</h1>
+          <hr />
+          <p className="text-xl text-gray-800">
+            <strong className="font-semibold">Name:</strong> {biodata.name}
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Date of Birth:</strong>
+            {biodata.dateOfBirth}
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Height:</strong> {biodata.height}
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Weight:</strong> {biodata.weight}
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Age:</strong> {biodata.age}
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Occupation:</strong>{" "}
+            {biodata.occupation}
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Race:</strong> {biodata.race}
+          </p>
+        </div>
+
+        {/* contact details */}
+        <div className="w-full bg-white shadow-lg px-8 py-5 rounded-lg">
+          <h1 className="text-2xl text-center">Contact Details</h1>
+          <hr />
+          <button className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg  px-5 py-2.5 text-center mt-2 mb-2 text-xl flex gap-2">
+            <img src={daimond} alt="" className="w-8" /> Request Contact
+            Information
+          </button>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Contact Email:</strong>{" "}
+            {biodata.email}
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Mobile Number:</strong>{" "}
+            {biodata.mobileNumber}
+          </p>
+        </div>
+        {/* address details */}
+        <div className="w-full bg-white shadow-lg px-8 py-5 rounded-lg">
+          <h1 className="text-2xl text-center">Adress Details</h1>
+          <hr />
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Permanent Division:</strong>{" "}
+            {biodata.permanentDivision}
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Present Division:</strong>{" "}
+            {biodata.presentDivision}
+          </p>
+        </div>
+
+        {/* partner details */}
+        <div className="w-full bg-white shadow-lg px-8 py-5 rounded-lg">
+          <h1 className="text-2xl text-center">Expected Partner Details</h1>
+          <hr />
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold">Expected Partner Age:</strong>{" "}
+            {biodata.partnerAge}
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold mr-2">
+              Expected Partner Height:
+            </strong>
+            {biodata.partnerHeight} inch
+          </p>
+          <p className="text-lg text-gray-800">
+            <strong className="font-semibold mr-2">
+              Expected Partner Weight:
+            </strong>
+            {biodata.partnerWeight} kg
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BiodataDetails;
